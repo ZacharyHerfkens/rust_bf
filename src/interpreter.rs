343207eada8ecd7f690e9f51d9aa2bd0b7b50349
Zachary Herfkens <zacharyherfkens1@gmail.com>
2023-08-22 11:45:42 -0700

    added error handling

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,51 +1,95 @@
-use std::io::{Read, Write};
+use std::io::{Read, Write, ErrorKind};
 
 use crate::parser::Instruction;
 
 
-pub fn run<R: Read, W: Write>(program: &[Instruction], input: R, mut output: W) -> Result<(), String> {
-    let mut memory = [0u8; 2_usize.pow(16)];
-    let mut ptr = 0;
+pub fn run<R: Read, W: Write>(program: &[Instruction], mut input: R, mut output: W) -> Result<(), String> {
+    let mut memory = Memory::new(2_usize.pow(16));
     let mut ip = 0;
-
-    let mut input_bytes = input.bytes();
 
     while let Some(instruction) = program.get(ip) {
         ip += 1;
         match instruction {
             Instruction::IncCell => {
-                memory[ptr] = memory[ptr].wrapping_add(1);
+                memory.add_cell(1);
             }
             Instruction::DecCell => {
-                memory[ptr] = memory[ptr].wrapping_sub(1);
+                memory.add_cell(-1);
             }
             Instruction::IncPtr => {
-                ptr += 1;
+                memory.move_ptr(1);
             }
             Instruction::DecPtr => {
-                ptr -= 1;
+                memory.move_ptr(-1);
             }
             Instruction::JumpIfZero(pos) => {
-                if memory[ptr] == 0 {
+                if memory.get_cell() == 0 {
                     ip = *pos;
                 }
             }
             Instruction::JumpIfNotZero(pos) => {
-                if memory[ptr] != 0 {
+                if memory.get_cell() != 0 {
                     ip = *pos;
                 }
             }
             Instruction::Read => {
-                let byte = input_bytes.next().unwrap_or(Ok(0)).map_err(|e| e.to_string())?;
-                memory[ptr] = byte;
+                memory.set_cell(read_byte(&mut input)?);
             }
             Instruction::Write => {
-                output
-                    .write_all(&[memory[ptr]])
-                    .map_err(|e| e.to_string())?;
+                write_byte(&mut output, memory.get_cell())?;
             }
         }
     }
 
     Ok(())
+}
+
+pub fn write_byte<W: Write>(mut w: W, byte: u8) -> Result<(), String> {
+    w.write_all(&[byte]).map_err(|e| e.to_string())
+}
+
+pub fn read_byte<R: Read>(mut r: R) -> Result<u8, String> {
+    let mut buf = [0; 1];
+    match r.read_exact(&mut buf) {
+        Ok(_) => {},
+        Err(ref e) if e.kind() == ErrorKind::UnexpectedEof => return Ok(0),
+        Err(e) => return Err(e.to_string()),
+    }
+    Ok(buf[0])
+}
+
+struct Memory {
+    cells: Vec<u8>,
+    ptr: usize,
+}
+
+impl Memory {
+    pub fn new(size: usize) -> Self {
+        Self {
+            cells: vec![0; size],
+            ptr: 0,
+        }
+    }
+
+    pub fn move_ptr(&mut self, offset: isize) {
+        let new_ptr = self.ptr.checked_add_signed(offset);
+        match new_ptr {
+            Some(p) => self.ptr = p,
+            None => panic!("Pointer out of bounds"),
+        }
+    }
+
+    pub fn add_cell(&mut self, val: i8) {
+        let cell = self.cells.get_mut(self.ptr).unwrap();
+        *cell = cell.wrapping_add(val as u8);
+    }
+
+    pub fn get_cell(&mut self) -> u8 {
+        *self.cells.get(self.ptr).unwrap()
+    }
+
+    pub fn set_cell(&mut self, val: u8) {
+        let cell = self.cells.get_mut(self.ptr).unwrap();
+        *cell = val;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,37 @@ use clap::Parser;
 #[derive(Parser)]
 struct BFApp {
     source: String,
-    
+
     #[arg(short, long)]
     input: Option<String>,
 }
 
 fn main() {
     let args = BFApp::parse();
-    let source = std::fs::read_to_string(args.source).unwrap();
-    let program = parser::parse(source.as_str()).unwrap();
     let input_string = args.input.unwrap_or_default();
     let input = input_string.as_bytes();
     let output = std::io::stdout();
 
-    interpreter::run(&program, input, output).unwrap();
+    let source = match std::fs::read_to_string(&args.source) {
+        Ok(source) => source,
+        Err(e) => {
+            eprintln!("File Error: {}", e);
+            return;
+        }
+    };
+
+    let program = match parser::parse(&source) {
+        Ok(program) => program,
+        Err(e) => {
+            eprintln!("Parsing Error: {}", e);
+            return;
+        }
+    };
+
+    if let Err(e) = interpreter::run(&program, input, output) {
+        eprintln!("Interpreter Error: {}", e);
+        return;
+    }
+
     println!();
 }
